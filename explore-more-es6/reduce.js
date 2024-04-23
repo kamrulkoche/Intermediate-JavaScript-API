8483d6db5f32500665e6a851eec7bfbc46f20fde
kamrulkoche <kamrulkoche@gmail.com>
2024-04-23 12:25:46 +0600

    reduce

---
+++ explore-more-es6/reduce.js
@@ -0,0 +1,6 @@
+const numbers = [4, 7, 9, 10];
+const total = numbers.reduce((previous, current) => previous + current, 0);
+console.log(total);
+
+const sum = numbers.reduce((p, c) => p + c, 0);
+console.log(sum);
