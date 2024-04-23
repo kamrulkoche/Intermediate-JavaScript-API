e4d85c9386ee2be5c433298013945f9628e6007d
kamrulkoche <kamrulkoche@gmail.com>
2024-04-23 11:48:55 +0600

    more map

---
+++ explore-more-es6/more-map.js
@@ -0,0 +1,9 @@
+const numbers = [4, 7, 9, 10];
+const doubled = numbers.map((num) => num * 2);
+//console.log(doubled);
+
+const friends = ["Tom", "Asik", "Reyad", "Alif"];
+const lengths = friends.map((friend) => friend.length);
+//console.log(lengths);
+const firstLetters = friends.map((firstLetter) => firstLetter[0]);
+console.log(firstLetters);
