b554df544c9d6b02289f718fcbd46c4f2d0b5c36
kamrulkoche <kamrulkoche@gmail.com>
2024-04-23 12:10:30 +0600

    find

---
+++ explore-more-es6/find.js
@@ -0,0 +1,12 @@
+//find ekti value return korbe r filter akadik value return korbe [array] return korbe .
+
+const players = [4, 75, 65, 57, 69, 74];
+const selected = players.find((players) => players > 60);
+console.log(selected);
+
+/**
+ * 1.map
+ * 2.forEach
+ * 3.filter
+ * 4.find
+ */
