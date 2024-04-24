76555766ae1d37797e9bdf5ff1d5759ba96a4da6
kamrulkoche <kamrulkoche@gmail.com>
2024-04-24 22:14:55 +0600

    fetch and app user

---
+++ explore-API/js/app.js
@@ -0,0 +1,16 @@
+function loadData2() {
+  fetch("https://jsonplaceholder.typicode.com/todos/1")
+    .then((res) => res.json())
+    .then((data) => console.log(data));
+}
+
+function loadUser() {
+  fetch("https://jsonplaceholder.typicode.com/users/1/posts")
+    .then((res) => res.json())
+    // .then(data=>console.log(data))
+    .then((data) => display(data));
+}
+
+function display(data) {
+  console.log(data);
+}
