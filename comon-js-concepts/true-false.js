fc032b8767f63bcb2640c0ef3fdee622e72b5135
kamrulkoche <kamrulkoche@gmail.com>
2024-04-24 11:34:48 +0600

    true-false

---
+++ comon-js-concepts/true-false.js
@@ -0,0 +1,35 @@
+/*
+Truthy:
+1.true
+2.any number (+ve, -ve) will be truthy other than 0
+3.any string other than empty string 
+4.'0' ,'false'
+5.{},[]
+Falsy:
+1.false
+2.0
+3.'' (empty string)
+4.undefined
+5.null
+
+*/
+
+const x = false;
+// if (x) {
+//   console.log("Value of x is truthy");
+// } else {
+//   console.log("Value of x is falsy");
+// }
+
+//optional
+//check falsy
+const y = "";
+if (!y) {
+  //console.log('Value is falsy');
+}
+
+//check true
+const z=''
+if(!z){
+    console.log('Value is truthy');
+}
