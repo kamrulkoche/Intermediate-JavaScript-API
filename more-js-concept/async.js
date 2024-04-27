3686b1c7834729e367ce3d2c865c0e01dd6f1311
kamrulkoche <kamrulkoche@gmail.com>
2024-04-28 00:01:48 +0600

    async add

---
+++ more-js-concept/async.js
@@ -0,0 +1,36 @@
+const myLoader = () => {
+  return new Promise((resolve, reject) => {
+    const success = Math.random();
+    if (success <= 0.5) {
+      resolve(success);
+      console.log("resolve");
+    } else {
+      reject(success);
+      console.log("reject");
+    }
+  });
+};
+
+myLoader()
+  .then((data) => console.log("resolved data", data))
+  .catch((err) => console.log("rejected with value", err));
+
+fetch("")
+  .then((res) => res.json())
+  .then((data) => console.log(data))
+  .catch((err) => console.log(err));
+
+async function loadData() {
+  const res = await fetch("");
+  const data = await res.json();
+  console.log(data);
+}
+
+loadData();
+
+
+const taskLoader=async()=>{
+    const res=await fetch('');
+    const data = await res.json();
+    console.log(data);
+}
